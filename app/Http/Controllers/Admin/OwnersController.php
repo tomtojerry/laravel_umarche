95f0b97b9a8e6fe9e11c3919a070495724bf8106
tominaga.d <zayntom14@gmail.com>
2022-04-13 21:31:01 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; // Eloquent エロクアント
+use Illuminate\Support\Facades\DB; // QueryBuilder クエリビルダー
+use Carbon\Carbon;
 
 class OwnersController extends Controller
 {
@@ -12,14 +15,24 @@ class OwnersController extends Controller
         $this->middleware('auth:admin');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        dd('オーナー一覧です');
+        $date_now = Carbon::now();
+        $date_parse = Carbon::parse(now());
+        echo $date_now;
+        echo $date_parse;
+
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $q_first = DB::table('owners')->select('name')->first();
+
+        // $c_test = collect([
+        //     'name' => 'テスト',
+        // ]);
+
+        // dd($e_all, $q_get, $q_first, $c_test);
+        return view('admin.owners.index',
+            compact('e_all', 'q_get'));
     }
 
     /**
