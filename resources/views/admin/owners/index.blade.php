589ac5c30d1e287de13b66838ae1106543e05a8c
tominaga.d <zayntom14@gmail.com>
2022-04-14 18:30:07 +0900

    pagination

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -60,6 +60,7 @@
 																				@endforeach
 																		</tbody>
 																</table>
+                                                                {{ $owners->links() }}
 														</div>
 												</div>
 										</section>
