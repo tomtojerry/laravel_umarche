2945f14b3349fa430e3e0adc5aed2d7a83f20b8d
tominaga.d <zayntom14@gmail.com>
2022-04-16 10:08:13 +0900

    リサイズなしのアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -38,10 +39,17 @@ class ShopController extends Controller
     }
 
     public function edit($id) {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id) {
+        $imageFile = $request->image;
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            Storage::putFile('public/shops', $imageFile);
+        }
 
+        return redirect()->route('owner.shops.index');
     }
 }
