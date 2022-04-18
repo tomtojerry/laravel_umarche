d17c93b8de2f14ad09317b9407a0b072aa09ba67
tominaga.d <zayntom14@gmail.com>
2022-04-18 17:56:17 +0900

    product初期設定

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -22,4 +22,8 @@ class Shop extends Model
     public function owner() {
         return $this->belongsTo(Owner::class);
     }
+
+    public function product() {
+        return $this->hasMany(Product::class);
+    }
 }
