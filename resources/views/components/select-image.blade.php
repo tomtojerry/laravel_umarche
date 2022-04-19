ead6f24bad02714316a299bf1bf17199753df6c6
tominaga.d <zayntom14@gmail.com>
2022-04-19 12:11:06 +0900

    product create

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -3,9 +3,10 @@
     if ($name === 'image2') { $modal = 'modal-2'; }
     if ($name === 'image3') { $modal = 'modal-3'; }
     if ($name === 'image4') { $modal = 'modal-4'; }
+    if ($name === 'image5') { $modal = 'modal-5'; }
 @endphp
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-        <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+        <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
                 <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
                         <header class="modal__header">
                                 <h2 class="text-xl text-gray-700" id="{{ $modal }}-title">
@@ -40,7 +41,7 @@
 </div>
 
 <div class="flex justify-around items-center mb-4">
-    <a href="#" data-micromodal-trigger="{{ $modal }}">ファイルを選択</a>
+    <a class="py-2 px-4 bg-gray-200" href="#" data-micromodal-trigger="{{ $modal }}">ファイルを選択</a>
     <div class="w-1/4">
         <img src="" alt="" id="{{ $name }}_thumbnail">
     </div>
