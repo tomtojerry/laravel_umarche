dcb87355ef2b499eb24244b12380b0a7a5394fe1
tominaga.d <zayntom14@gmail.com>
2022-04-19 15:58:46 +0900

    product edit

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -4,6 +4,8 @@
     if ($name === 'image3') { $modal = 'modal-3'; }
     if ($name === 'image4') { $modal = 'modal-4'; }
     if ($name === 'image5') { $modal = 'modal-5'; }
+    $cImage = $currentImage ?? '';
+    $cId = $currentId ?? '';
 @endphp
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
         <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
@@ -43,7 +45,7 @@
 <div class="flex justify-around items-center mb-4">
     <a class="py-2 px-4 bg-gray-200" href="#" data-micromodal-trigger="{{ $modal }}">ファイルを選択</a>
     <div class="w-1/4">
-        <img src="" alt="" id="{{ $name }}_thumbnail">
+        <img id="{{ $name }}_thumbnail" @if($cImage) src="{{ asset('storage/products/' . $cImage) }}" @else src="" @endif>
     </div>
 </div>
-<input type="hidden" id="{{ $name }}_hidden" name="{{ $name }}" value="">
+<input type="hidden" id="{{ $name }}_hidden" name="{{ $name }}" value="{{ $cId }}">
