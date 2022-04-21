1a31e5924072e834a8711e8d68c9326b4bd70e46
tominaga.d <zayntom14@gmail.com>
2022-04-21 23:21:33 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -44,12 +49,18 @@ class ItemController extends Controller
                 'products.information',
                 'secondary_categories.name as category',
                 'image1.filename as filename'
-            ) 
+            )
             ->get();
 
         // dd($stocks, $products);
         // $products = Product::all();
 
         return view('user.index', compact('products'));
+    }
+
+    public function show($id) {
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
     }
 }
