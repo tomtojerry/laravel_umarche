9ca862c7fc1cdf7d1b55022d4856ac7eae5b6902
tominaga.d <zayntom14@gmail.com>
2022-04-22 14:12:52 +0900

    cart add

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
 use App\Http\Controllers\User\ItemController;
-
+use App\Http\Controllers\User\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +26,12 @@ Route::middleware('auth:users')->group(function () {
     Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
 
+Route::prefix('cart')
+    ->middleware('auth:users')->group(function () {
+        Route::post('/add', [CartController::class, 'add'])->name('cart.add');
+});
+
+
 // Route::get('/dashboard', function () {
 //     return view('user.dashboard');
 // })->middleware(['auth:users'])->name('dashboard');
@@ -36,4 +42,4 @@ Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServic
 Route::get('/serviceprovidertest', [LifeCycleTestController::class, 'showServiceProviderTest']);
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
