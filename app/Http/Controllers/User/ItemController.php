68342419b8775cd9f612b8294c57f87a1bfdeea6
tominaga.d <zayntom14@gmail.com>
2022-04-23 10:49:40 +0900

    表示件数

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -31,7 +31,7 @@ class ItemController extends Controller
     {
         $products = Product::AvailableItems()
             ->sortOrder($request->sort)
-            ->get();
+            ->paginate($request->pagination);
 
         return view('user.index', compact('products'));
     }
