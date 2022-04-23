7094fbad692e2b01a6c8a3a53595597287087a14
tominaga.d <zayntom14@gmail.com>
2022-04-23 14:42:53 +0900

    cartService作成

---
+++ app/Services/CartService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Product;
+use App\Models\Cart;
+
+class CartService
+{
+    public static function getItemsInCart($items)
+    {
+        $products = [];
+
+        foreach ($items as $item) {
+            $p = Product::findOrFail($item->product_id);
+            $owner = $p->shop->owner->select('name', 'email')->first()->toArray();
+            $values = array_values($owner);
+            $keys = ['ownerName', 'email'];
+            $ownerInfo = array_combine($keys, $values);
+
+            $product = Product::where('id', $item->product_id)
+                ->select('id', 'name', 'price')->get()->toArray(); // 商品情報の配列
+            $quantity = Cart::where('product_id', $item->product_id)
+                ->select('quantity')->get()->toArray(); // 在庫数の配列
+
+            $result = array_merge($product[0], $ownerInfo, $quantity[0]);
+
+            array_push($products, $result);
+        }
+        dd($products);
+
+        return $products;
+    }
+}
